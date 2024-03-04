05bd4f8fe8e5ceb63ad252d25a10f1494c0e10bb
Dev-Olena <alenagruda@gmail.com>
2024-03-04 14:38:17 +0200

    populate()

--- controllers/Rent.controller.js
+++ controllers/Rent.controller.js
@@ -13,8 +13,10 @@ module.exports.createOne = async (req, res, next) => {
 module.exports.getOne = async (req, res, next) => {
     try {
         const {params: {rentId}} = req;
-        const rent = await Rent.findById(rentId);
-        res.status(201).send({data: rent});        
+        const rent = await Rent.findById(rentId).
+            populate('movie').
+            populate('user');
+        res.status(200).send({data: rent});        
     } catch (error) {
         next(error)
     }
@@ -24,9 +26,20 @@ module.exports.update = async (req, res, next) => {
     try {
         const {body, params: {rentId}} = req;
         const updated = await Rent.FindByIdAndUpdate(rentId, body, {returnDocument: 'after'});
-        res.status(201).send({data: updated});        
+        res.status(200).send({data: updated});        
     } catch (error) {
         next(error)
     }
 };
+
+module.exports.getAllUserRents = async (req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        const rents = await Rent.find({user: userId}).populate('user').populate('movie');
+        res.status(200).send({data: rents});
+
+    } catch (error) {
+        next(error)
+    }
+}
 
